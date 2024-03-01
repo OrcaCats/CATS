8644e0161c33196d7050923839310528c42a6d1a
Tensor-6 <124675744+Tensor-6@users.noreply.github.com>
2024-03-01 11:25:36 -0800

    Depenmdancy installer

--- import sys.py
+++ import sys.py
@@ -1,14 +1,11 @@
 #dependancy import
 import sys
 import subprocess
-
 def installPack(depens):
-    for i, j in enumerate(depens):
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', j])
-        return f'installed {i+1} packages'
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', depens])
 #dependancy list
-dependancies = ['torch']
-print(installPack(dependancies))
+dependancies = ['keras-ocr']
+#print(installPack(dependancies))
+for pack in dependancies:
+    installPack(pack)
 #------------------------------------------------------------------------------------
-
-#
