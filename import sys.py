873bc408851e14189b8225b628a629e2d95af29c
Tensor-6 <124675744+Tensor-6@users.noreply.github.com>
2024-03-01 13:23:15 -0800

    Comp;letion of dependancy downloader

--- import sys.py
+++ import sys.py
@@ -1,15 +1,40 @@
-#dependancy import
+#functionize
 import sys
 import subprocess
-def installPack(depens):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', depens])
-#dependancy list
-dependancies = ['keras-ocr', 'tensorflow']
-#print(installPack(dependancies))
-for pack in dependancies:
-    installPack(pack)
-#------------------------------------------------------------------------------------
+try:
+    import platform
+except ImportError:
+    print('Module platform not found')
+def install_pack(dependency):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', dependency])
+    print(f'Successfully installed: {dependency}')
+# Dependency list
+dependencies = ['keras-ocr',     #text recognizer
+                'pyautogui',     #screenshoter
+                'pynput'         #input listener
+                ]
+for package in dependencies:
+    install_pack(package)
+print(f'Platform name: {platform.system()}')
 
-import keras_ocr
-import matplotlib.pyplot as plt
-pipeline = keras_ocr.pipeline.Pipeline()
+#------------------------------------------------------------------------------------
+#inplement check system exception
+
+class screenshot:
+    def __init__(self):
+        import pyautogui
+        import pynput
+        self.cords[0, 0, 0, 0]
+        self.screenshot
+    def logCords(self, cords):
+        listener = 
+        
+    def shoot(self, cords):
+        self.screenshot = pyautogui.screenshot(region=(x, y, w, h))
+        
+class recog:
+    def __init__(self):
+        import keras_ocr
+        import matplotlib.pyplot as plt
+        pipeline = keras_ocr.pipeline.Pipeline()
+
