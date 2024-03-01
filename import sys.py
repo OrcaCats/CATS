3630fbba122e17f97a0d009c37b14f5982af7b31
Tensor-6 <124675744+Tensor-6@users.noreply.github.com>
2024-03-01 11:33:51 -0800

    wfe

--- import sys.py
+++ import sys.py
@@ -4,8 +4,12 @@ import subprocess
 def installPack(depens):
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', depens])
 #dependancy list
-dependancies = ['keras-ocr']
+dependancies = ['keras-ocr', 'tensorflow']
 #print(installPack(dependancies))
 for pack in dependancies:
     installPack(pack)
 #------------------------------------------------------------------------------------
+
+import keras_ocr
+import matplotlib.pyplot as plt
+pipeline = keras_ocr.pipeline.Pipeline()
