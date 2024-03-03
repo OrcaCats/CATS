e563ad2cafcc782fe9cf3ab29968dddc71cba88b
Tensor-6 <Kelper22c@gmail.com>
2024-03-03 15:34:34 -0800

    Class Screenshot Update

--- CATS.py
+++ CATS.py
@@ -6,22 +6,6 @@ dependencies = ['easyocr',     #primary text recognizer
                 'mouse'
                 ]
 
-class screenshot:
-    def __init__(self):
-        #imports within function for procedural purposes
-        global pyautogui
-        global pynput
-        import pyautogui
-        import pynput
-        self.cords[0, 0, 0, 0]
-        self.screenshot
-    def logCords(self, cords):
-        self.logger = mouseCords()
-
-        #listener = 
-        
-    def shoot(self, cords):
-        self.screenshot = pyautogui.screenshot(region=(x, y, w, h))
 
 
 #add runtime tracker
@@ -29,12 +13,15 @@ class mouseLogger:
     def __init__(self):
         self.running = False
         self.runtimes = 0
+        self.cords = []
     def start(self):
         if not self.running:
             self.running = True
             keyboard.hook(self.onKey)
-            keyboard.wait('s')
-            self.stop
+            #keyboard.wait('s')
+            while self.runtimes < 2: #stops after 2 cords
+                pass
+            self.stop()
     def stop(self):
         if self.running:
             self.running = False
@@ -42,9 +29,9 @@ class mouseLogger:
     def onKey(self, e):
         if e.event_type == keyboard.KEY_DOWN and e.name == 'a':
             mouse_position = get_position()
-            return mouse_position
+            #print(mouse_position)   #debug
+            self.cords.append(mouse_position) 
             self.runtimes += 1
-
 
 #primary reader
 class recogEasyOCR:
@@ -61,49 +48,34 @@ class recogKeras:
         pipeline = keras_ocr.pipeline.Pipeline()
 
 
+class screenshot:
+    def __init__(self):
+        #imports within function for procedural purposes
+        global pyautogui
+        global pynput
+        import pyautogui
+        import pynput
+        self.cords = [0, 0, 0, 0]
+    def logCords(self):
+        print('a')
+        self.logger = mouseLogger()
+        self.logger.start()
+        self.cords[0], self.cords[1], self.cords[2], self.cords[3]= self.logger.cords[0][0], self.logger.cords[0][1], self.logger.cords[1][0] - self.logger.cords[0][0], self.logger.cords[1][1] - self.logger.cords[0][1]
+    def shoot(self, cords):
+        self.screenshot = pyautogui.screenshot(region=(self.cords[0], self.cords[1], self.cords[2], self.cords[3]))
+
+
 if __name__=='__main__':
     from ImportTree import importDependencies
     #check dependancyes and install
     #importDependencies(dependencies)
 
+#
+
 import keyboard as keyboard
 from mouse import get_position
-logger = mouseLogger()
-logger.start()
-
-'''
-class mouseCords:
-    def __init__(self):
-        from pynput import mouse as mouse
-        from pynput import keyboard
-        self.listener = None
-        self.keyListener = keyboard.Listener(on_press=self.onAltQ) #sets up keyboard listener
-        self.startMouse()
-        #self.keyListener.start()
-        pass
-    
-    def startMouse(self): #different from init because of ctrl of listener. parent classes init
-        if self.listener is None:
-            self.listener = mouse.Listener()
-            print('Listener Started')
-            with self.listener as listener:
-                listener.join()
-
-    def stopMouse(self):
-        if self.listener is not None:
-            self.listener.stop()
-            self.listener = None #deinits listener function
-            print('spywa-- achem listener, stopped')
-
-    def onAltQ(self, key): #detects when alr r is pressed and activates mouse
-        try:
-            if key.char == 'q' and any([keyboard.Key.ctrl in key_modifiers for key_modifiers in key.modifiers]):
-                self.startMouse()
-        except AttributeError: #catches non-char keys
-            pass
-
-    def onClick(self, x, y, button, pressed):
-        if pressed:
-            print(f'click at {x}, {y}')
-            self.stopMouse()
-'''
+#logger = mouseLogger()
+#logger.start()
+#print(logger.cords)
+scr = screenshot()
+scr.logCords()
